c2c1d9974dc0f9e5e67dbf9535a8f7666a9e1d87
Joe Scott <jbs198585@gmail.com>
2022-03-02 08:23:57 -0500

    manipulation demo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const allLinks = document.querySelectorAll('a');
 
 for (let link of allLinks) {
-  link.innerText = 'I AM A LINK!!!!';
+  link.style.color = 'rgb(0, 108, 134)';
   link.style.textDecorationColor = 'magenta';
 };
